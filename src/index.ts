914ec812737e62bcde31afdcfef50f56e5a4b81d
RakhaTF <rakha.tradersfamily@gmail.com>
2023-08-10 18:39:10 +0700

    feat: :sparkles: Adding typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { AppDataSource } from "@infrastructure/mysql/connection";
 import fastify from "fastify";
 
 const server = fastify({
@@ -7,7 +8,14 @@ const server = fastify({
         }
     }
 });
-
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+    throw new Error("Failed to initialize database"); // Throw an error if initialization fails
+  });
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
